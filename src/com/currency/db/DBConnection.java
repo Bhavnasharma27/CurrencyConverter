b3bfdfd0874d09470fc432904a082c0c84b43538
GurvirSinghH <g2207552@gmail.com>
2026-05-22 11:59:55 +0530

    dbUpdate

--- src/com/currency/db/DBConnection.java
+++ src/com/currency/db/DBConnection.java
@@ -6,19 +6,19 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String HOST     = "localhost";
-    private static final String PORT     = "3306";
+    private static final String HOST = "localhost";
+    private static final String PORT = "3306";
     private static final String DATABASE = "currency_db";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "your_password";
+    private static final String PASSWORD = "Gurvip27@#";
 
-    private static final String URL =
-        "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE
-        + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE
+            + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
     private static Connection connection = null;
 
-    private DBConnection() {}
+    private DBConnection() {
+    }
 
     public static Connection getConnection() throws SQLException {
         try {
@@ -35,8 +35,11 @@ public class DBConnection {
 
     public static void closeConnection() {
         if (connection != null) {
-            try { connection.close(); }
-            catch (SQLException e) { System.err.println(e.getMessage()); }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
